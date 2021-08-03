29d874d4692037663008b32e12a8468815ddc948
Abraham Avelino <20203tn072@utez.edu.mx>
2021-08-03 18:49:37 -0500

    Elaboracion del crud en el model

---
+++ src/main/java/mx/edu/utez/model/games/DaoGames.java
@@ -0,0 +1,143 @@
+package mx.edu.utez.model.games;
+
+import mx.edu.utez.model.category.BeanCategory;
+import mx.edu.utez.service.ConnectionMySQL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import java.sql.*;
+import java.util.*;
+
+public class DaoGames {
+    private Connection con;
+    private CallableStatement cstm;
+    private ResultSet rs;
+    final private Logger CONSOLE = LoggerFactory.getLogger(DaoGames.class);
+
+    public List<BeanGames> findAll(){
+        List<BeanGames> listGames = new ArrayList<>();
+
+        try{
+            con = ConnectionMySQL.getConnection();
+            cstm = con.prepareCall("{call sp_findGames}");
+            rs = cstm.executeQuery();
+
+            while (rs.next()){
+                BeanCategory beanCategory = new BeanCategory();
+                BeanGames beanGames = new BeanGames();
+
+                beanCategory.setIdCategory(rs.getInt("idCategory"));
+                beanCategory.setName(rs.getString("name"));
+                beanCategory.setStatus(rs.getInt("status"));
+
+                beanGames.setIdGames(rs.getInt("idGames"));
+                beanGames.setName(rs.getString("name"));
+                String photo = Base64.getEncoder().encodeToString(rs.getBytes("imgGames"));
+                beanGames.setdate_premiere(rs.getString("datePremiere"));
+                beanGames.setStatus(rs.getInt("status"));
+
+                beanGames.setCategory_idCategory(beanCategory);
+
+
+            }
+        }catch(SQLException e){
+            CONSOLE.error("Ha ocurrido un error: " + e.getMessage());
+        }finally {
+            ConnectionMySQL.closeConnections(con, cstm, rs);
+        }
+        return listGames;
+    }
+
+    public BeanGames findById(int idGames){
+        BeanGames beanGames = null;
+        try {
+            con = ConnectionMySQL.getConnection();
+            cstm = con.prepareCall("SELECT * FROM games AS G INNER JOIN category AS C ON G.idCategory_idCategory = C.idCategory WHERE G.idGame = ?");
+            cstm.setLong(1, idGames);
+            rs = cstm.executeQuery();
+
+            if (rs.next()){
+                BeanCategory beanCategory = new BeanCategory();
+                beanGames = new BeanGames();
+
+                beanCategory.setIdCategory(rs.getInt("idCategory"));
+                beanCategory.setName(rs.getString("name"));
+                beanCategory.setStatus(rs.getInt("status"));
+
+                beanGames.setIdGames(rs.getInt("idGames"));
+                beanGames.setName(rs.getString("name"));
+                String photo = Base64.getEncoder().encodeToString(rs.getBytes("imgGames"));
+                beanGames.setdate_premiere(rs.getString("datePremiere"));
+                beanGames.setStatus(rs.getInt("status"));
+
+                beanGames.setCategory_idCategory(beanCategory);
+            }
+        }catch (SQLException e){
+            CONSOLE.error("Ha ocurrido un error: " + e.getMessage());
+
+        }finally {
+            ConnectionMySQL.closeConnections(con, cstm, rs);
+        }
+        return beanGames;
+    }
+
+    public boolean create(BeanGames beanGames){
+        boolean flag = false;
+        try {
+            con = ConnectionMySQL.getConnection();
+            cstm = con.prepareCall("{call sp_create(?,?,?,?,?)}");
+
+            cstm.setString(1, beanGames.getCategory_idCategory().getName());
+            cstm.setString(2, beanGames.getName());
+            cstm.setString(3, beanGames.getImgGame());
+            cstm.setString(4, beanGames.getdate_premiere());
+            cstm.setInt(5, beanGames.getCategory_idCategory().getIdCategory());
+            cstm.execute();
+            flag = true;
+        }catch (SQLException e){
+            CONSOLE.error("Ha ocurrido un error: " + e.getMessage());
+        }finally {
+            ConnectionMySQL.closeConnections(con, cstm);
+        }
+        return flag;
+    }
+
+    public boolean update(BeanGames beanGames){
+        boolean flag = false;
+        try {
+            con = ConnectionMySQL.getConnection();
+            cstm = con.prepareCall("{call sp_update(?,?,?,?,?,?)}");
+
+            cstm.setString(1, beanGames.getCategory_idCategory().getName());
+            cstm.setString(2, beanGames.getName());
+            cstm.setString(3, beanGames.getImgGame());
+            cstm.setString(4, beanGames.getdate_premiere());
+            cstm.setInt(5, beanGames.getCategory_idCategory().getIdCategory());
+            cstm.setInt(6,beanGames.getIdGames());
+
+            flag = cstm.execute();
+        }catch (SQLException e){
+            CONSOLE.error("Ha ocurrido un error: " + e.getMessage());
+        }finally {
+            ConnectionMySQL.closeConnections(con, cstm);
+        }
+        return flag;
+    }
+
+    public boolean delete(long idGame) {
+        boolean flag = false;
+        try {
+            con = ConnectionMySQL.getConnection();
+            cstm = con.prepareCall("{call sp_delete(?)}");
+            cstm.setLong(1, idGame);
+
+            flag = cstm.execute();
+        } catch (SQLException e) {
+            CONSOLE.error("Ha ocurrido un error: " + e.getMessage());
+        } finally {
+            ConnectionMySQL.closeConnections(con, cstm);
+        }
+        return flag;
+    }
+}
---
+++ videogames.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-08-2021 a las 01:23:50
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `videogames`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_create` (IN `p_nameCategory` VARCHAR(255), IN `p_nameGame` VARCHAR(255), IN `p_imgGame` LONGBLOB, IN `p_date_premiere` VARCHAR(255), IN `p_idCategory` INT)  BEGIN
+    INSERT INTO category(name) VALUES(p_nameCategory);
+    INSERT INTO games(name, img_game, date_premiere, Category_idCategory) VALUES(p_nameGame, p_imgGame, p_date_premiere, p_idCategory);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_delete` (IN `p_idGame` INT)  BEGIN
+	UPDATE games SET status = 0 WHERE idGame = p_idGame;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_findGames` ()  BEGIN
+	SELECT * FROM games AS G INNER JOIN category AS C ON G.idCategory_idCategory = C.idCategory;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_update` (IN `p_name` VARCHAR(255), IN `p_name2` VARCHAR(255), IN `p_imgGame` LONGBLOB, IN `p_date_premiere` VARCHAR(255), IN `p_idCategory` INT, IN `p_idGame` INT)  BEGIN
+	DECLARE v_idGame INT;
+    SELECT C.idCategory FROM games AS G INNER JOIN category AS C ON G.Category_idCategory = C.idCategory WHERE G.idGame = p_idGame INTO v_idGame;
+    
+    UPDATE category SET name = p_name WHERE idCategory = p_idCategory;
+    
+    UPDATE games SET name = p_name2, img_game = p_imgGame, date_premiere = p_date_premiere,	Category_idCategory = p_idCategory WHERE idGame = p_idGame;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `category`
+--
+
+CREATE TABLE `category` (
+  `idCategory` int(11) NOT NULL,
+  `name` varchar(120) NOT NULL,
+  `status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `games`
+--
+
+CREATE TABLE `games` (
+  `idGame` int(11) NOT NULL,
+  `name` varchar(120) NOT NULL,
+  `img_game` longblob NOT NULL,
+  `date_premiere` date NOT NULL,
+  `Category_idCategory` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`idCategory`);
+
+--
+-- Indices de la tabla `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`idGame`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `category`
+--
+ALTER TABLE `category`
+  MODIFY `idCategory` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `games`
+--
+ALTER TABLE `games`
+  MODIFY `idGame` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
